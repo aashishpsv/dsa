625bb18a7b475d34bc0be9a5632393584c77991a
aashish <96538664+aashishpsv@users.noreply.github.com>
2024-07-30 11:15:08 +0530

    majorityele

---
+++ strivera2z/arrays/medium/majorityelement.java
@@ -0,0 +1,54 @@
+give the majority element with occurences in array >n/2
+
+logical : sort arry. and return arr[n/2];
+
+brute:cpunt all occurences. then check if > n/2. tc = o(n^2).
+ for (int i = 0; i < n; i++) {
+            //selected element is v[i]
+            int cnt = 0;
+            for (int j = 0; j < n; j++) {
+                // counting the frequency of v[i]
+                if (v[j] == v[i]) {
+                    cnt++;
+                }
+            }
+
+            // check if frquency is greater than n/2:
+            if (cnt > (n / 2))
+                return v[i];
+        }
+
+better:use of hashmap.
+//O(N*logN) + O(N), where N = size of the given array.
+Reason: We are using a map data structure. Insertion in the map takes logN time. And we are doing it for N elements. So, it results in the first term O(N*logN). The second O(N) is for 
+checking which element occurs more than floor(N/2) times. If we use unordered_map instead, the first term will be O(N) for the best and average case and for the worst case, it will be O(N2).
+
+Space Complexity: O(N) as we are using a map data structure.
+ //size of the given array:
+        int n = v.length;
+
+        //declaring a map:
+        HashMap<Integer, Integer> mpp = new HashMap<>();
+
+        //storing the elements with its occurnce:
+        for (int i = 0; i < n; i++) {
+            int value = mpp.getOrDefault(v[i], 0);
+            mpp.put(v[i], value + 1);
+        }
+
+        //searching for the majority element:
+        for (Map.Entry<Integer, Integer> it : mpp.entrySet()) {
+            if (it.getValue() > (n / 2)) {
+                return it.getKey();
+            }
+        }
+
+best : moore voting algo. tc: o(n)
+  int count = 0;
+        int cur = 0;
+        for(int i : nums){
+            if(count==0) cur = i;
+            if(i == cur) count++;
+            else count--;
+        }
+        return cur;
