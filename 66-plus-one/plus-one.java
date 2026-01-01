cf3bbf7d49e7334619acf63be32fd884e0380227
aashish <96538664+aashishpsv@users.noreply.github.com>
2026-01-01 21:55:56 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (37.47%) - LeetSync

--- 66-plus-one/plus-one.java
+++ 66-plus-one/plus-one.java
@@ -1,7 +1,20 @@
 class Solution {
     public int[] plusOne(int[] digits) {
         int size = digits.length;
-        if(digits[size-1]!=9){
+        int i = size-1;
+
+        while(i>=0){
+            if(digits[i] < 9){
+                digits[i]++;
+                return digits;
+            }
+            digits[i]=0;
+            i--;
+        }
+        int[] newD = new int[size+1];
+        newD[0] = 1;
+        return newD;
+      /*  if(digits[size-1]!=9){
             digits[size-1] +=1;
           return digits;
         }
@@ -18,6 +31,6 @@ class Solution {
            }
         int[] newD = new int[size+1];
         newD[0] = 1;
-        return newD;
+        return newD; */
         }
     }
