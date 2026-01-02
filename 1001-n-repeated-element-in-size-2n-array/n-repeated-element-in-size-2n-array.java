ab9190a0447b1f68de7c10f4df8b2348f773ec94
aashish <96538664+aashishpsv@users.noreply.github.com>
2026-01-02 21:29:40 +0530

    Time: 11 ms (39.27%) | Memory: 47.8 MB (6.11%) - LeetSync

--- 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.java
+++ 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.java
@@ -3,7 +3,7 @@ class Solution {
         int x = nums.length/2;
        Arrays.sort(nums);
        for(int i=0;i<x*2;i++){
-        if(nums[i+1]-nums[i] == 0)return nums[i+1];
+        if(nums[i+1]-nums[i] == 0)return nums[i];
        }
 
        return 0;
